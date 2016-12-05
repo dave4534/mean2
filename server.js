68e041f32314585ccdf1fbf173e4ae4703bc2bcb
Dave <uDave@Dave.local>
2016-12-05 23:09:12 +0200

    Need to display data in client

--- server.js
+++ server.js
@@ -12,15 +12,22 @@ app.use('/node_modules', express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// 
+// 1. Create/Post request todoItems from server via req.body
 app.post('/todos', function(req, res){
   var todoItem = new todo(req.body);
-
+  // Save the todoItems and send them to client?
   todoItem.save(function(err, todoItem){
     console.log("Error: "+err);
     console.log("Yay"+todoItem);
     res.send(todoItem);
   });
+
+  app.get('/todos', function(req, res){
+  todoItem.find(function(error, beers){
+    res.send(todo);
+    });
+  });
+
 });
 
 app.listen(3000);
